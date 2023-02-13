3bad3f8497e288154945a90f495db4b0809fa424
@yang-yucheng03 <yangyucheng3@h-partners.com>
2023-02-13 19:16:10 +0800

    “jan”

---
+++ src/callback.py
@@ -0,0 +1,15 @@
+"""
+@author:32369
+@file:callback.py
+@time:2021/04/03
+Defined callback for DeepFFM.
+"""
+import time
+
+import numpy as np
+from mindspore import Tensor
+from mindspore.train.callback import Callback
+import moxing as mox
+
+
+
